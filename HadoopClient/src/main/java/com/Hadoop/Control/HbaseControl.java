7787ab321e9bdfb48da2ebdf2fbd18853beb231e
jinx <jinxh29224@hs.handsome.com.cn>
2020-07-29 17:24:16 +0800

    client update func add,copy

--- HadoopClient/src/main/java/com/Hadoop/Control/HbaseControl.java
+++ HadoopClient/src/main/java/com/Hadoop/Control/HbaseControl.java
@@ -6,19 +6,70 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import com.Hadoop.Utils.RequestUtils;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.Hadoop.Log.*;
 
 @Controller
 public class HbaseControl {
 	
 	@Autowired
 	com.Hadoop.Service.HbaseService HbaseService;
-	@RequestMapping("/hbasesearch")
+	
+	@Autowired
+	Info Info;
+	
+	@Autowired
+	RequestUtils RequestUtils;
+	
+	@RequestMapping("/SearchHbase")
 	@ResponseBody
-	public String hbasesearch(HttpServletRequest request) {
-		return HbaseService.SearchTable(request.getParameter("TabelName").trim(), request.getParameter("Rowkey").trim(), request.getParameter("zkip").trim(), request.getParameter("zkport").trim());
+	public String SearchHbase(HttpServletRequest request) {
+		try {
+		JSONObject json=RequestUtils.toJsonObject(request);
+		return HbaseService.SearchTable(json.getString("TableName").trim(),json.getString("Rowkey").trim()==null?"":json.getString("Rowkey").trim(),json.getString("zkip").trim(),json.getString("zkport").trim());
+		}catch(Exception e) {
+			return e.getMessage();
+		}
 	}
-	@RequestMapping("/hbaseclient")
-	public String hbaseclient(HttpServletRequest request) {
+	@RequestMapping("/HbaseClient")
+	public String HbaseClient() {
 		return "HbaseClient";
 	}
+	
+	
+	@RequestMapping("/Tabletransfer")
+	@ResponseBody
+	public String Tabletransfer(HttpServletRequest request) {
+		try {
+			JSONObject json=RequestUtils.toJsonObject(request);
+			System.out.println(json);
+			return HbaseService.InTableDatatransfer(json.getString("OrginTable").trim(),json.getString("DestTable").trim(),json.getString("zkip").trim(),json.getString("zkport").trim());
+			}catch(Exception e) {
+				return e.getMessage();
+			}
+		
+	}
+	
+	@RequestMapping(value="/AddHbase",produces = "application/json;charset=UTF-8")
+	@ResponseBody
+	public String AddhHbase(HttpServletRequest request) {
+		try {
+		JSONObject json=RequestUtils.toJsonObject(request);
+	    JSONArray jr=new JSONArray();
+		String insertjson=json.getString("Insertdata");
+		if(insertjson.startsWith("{")&&insertjson.endsWith("}")) {
+			jr.add(JSONObject.parse(insertjson));
+		}else {
+			jr=JSONArray.parseArray(insertjson);
+		}
+		return HbaseService.InsertTable(json.getString("TableName").trim(),json.getString("Rowkey").trim(),json.getString("zkip").trim(),json.getString("zkport").trim(),jr);
+		
+		}catch(Exception e) {
+			return Info.toJson(e.getMessage(), "fail");
+		}
+}
+
+	
 }
--- HadoopClient/src/main/java/com/Hadoop/Service/HbaseService.java
+++ HadoopClient/src/main/java/com/Hadoop/Service/HbaseService.java
@@ -1,13 +1,20 @@
 package com.Hadoop.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 import java.io.IOException;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.commons.collections.map.LinkedMap;
 import org.apache.hadoop.conf.Configuration;
@@ -18,17 +25,29 @@ import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
 import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.ResultScanner;
 import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.hbase.KeyValue;  
-
+import com.Hadoop.Log.*;
 
 @Service
 public class HbaseService {
+	@Autowired
+	Info Info;
 	
+	
+	/**
+	 * 查找hbase表中数据返回json
+	 * @param tablename
+	 * @param rowkey
+	 * @param zkip
+	 * @param zkport
+	 * @return
+	 */
 	public String  SearchTable(String tablename,String rowkey,String zkip,String zkport) {
 		try {
 		if(rowkey.equals("")) {
@@ -40,10 +59,42 @@ public class HbaseService {
 		}
 		
 	}
- 
 	
+	public String InsertTable(String tablename,String rowkey,String zkip,String zkport,JSONArray jr) {
+		Connection conn = null;
+		try {
+		    conn=initHbase(zkip,zkport);
+			Table table=initHbase(zkip,zkport).getTable(TableName.valueOf(tablename));
+			Put put=new Put(rowkey.getBytes());
+			long timestamp=new Date().getTime();
+		    for(int i=0;i<jr.size();i++) {
+		    	JSONObject json=jr.getJSONObject(i);
+		    	Set<String> sets=json.keySet();
+		    	sets.forEach(s->{
+		    	 	put.addColumn(Bytes.toBytes(json.getString("Family")==null?"default":json.getString("Family")),Bytes.toBytes(s),timestamp, Bytes.toBytes(json.getString(s)));
+		    	});
+		    }
+		    table.put(put);
+		    return Info.toJson("新增成功", "success");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return Info.toJson( e.getMessage(), "fail");
+		} finally {
+			try {
+				conn.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+	
+
 	private String SearchAllTable(String tablename,String zkip,String zkport) throws IOException { 
-		Table table=initHbase(zkip,zkport).getTable(TableName.valueOf(tablename)); 
+		Connection conn = null;
+		try {
+		conn=initHbase(zkip,zkport);
+		Table table=conn.getTable(TableName.valueOf(tablename)); 
 		 Scan scan = new Scan();
 		 ResultScanner scanner = table.getScanner(scan);
 		 Map<String,Map<String,String>> result=new LinkedMap();
@@ -58,12 +109,24 @@ public class HbaseService {
 		    	 }
 	               });
 	        }
-		 return JSON.toJSONString(result);
+		return JSON.toJSONString(result);
+		}catch(Exception e) {
+			return e.getMessage();
+		}finally {
+			conn.close();
+			
+		}
+		
 	}
 	
 	
 	private String SearchTableRowkey(String tablename,String rowkey,String zkip,String zkport) throws IOException {
-		 Table table=initHbase(zkip,zkport).getTable(TableName.valueOf(tablename)); 
+		
+		Connection conn = null;
+		try {
+	
+		conn=initHbase(zkip,zkport);
+		Table table=conn.getTable(TableName.valueOf(tablename)); 
 		 Get g = new Get(Bytes.toBytes(rowkey));
 		 Result r=table.get(g); 
 		 Map<String,Map<String,String>> result=new LinkedMap();
@@ -76,8 +139,46 @@ public class HbaseService {
 	    		 result.put(Bytes.toString(CellUtil.cloneRow(cell)), map);
 	    	 }
                });
-      
-		return   JSON.toJSONString(result);
+			return JSON.toJSONString(result);
+			}catch(Exception e) {
+				return e.getMessage();
+			}finally {
+				conn.close();
+				
+			}
+	}
+	
+	/**
+	 * 同集群不同表数据转移
+	 * @throws IOException 
+	 */
+	public  String InTableDatatransfer(String orgintable,String desttable,String zkip,String zkport) throws IOException {
+
+		Connection conn = null;
+		try {
+		conn=initHbase(zkip,zkport);
+		Table  otable=conn.getTable(TableName.valueOf(orgintable)); 
+		Table dtable=conn.getTable(TableName.valueOf(desttable)); 
+		 Scan scan = new Scan();
+		 ResultScanner scanner = otable.getScanner(scan);
+		 Map<String,Map<String,String>> result=new LinkedMap();
+		 List<Put> puts=new LinkedList<Put>();
+		 for (Result r :scanner){
+			 r.listCells().forEach(cell -> {
+					Put put=new Put(CellUtil.cloneRow(cell));
+					put.addColumn(CellUtil.cloneFamily(cell),CellUtil.cloneQualifier(cell),cell.getTimestamp(),CellUtil.cloneValue(cell));
+					puts.add(put);
+			 }
+			 );
+		 }
+		 dtable.put(puts); 
+		 return "拷贝成功";
+		}catch(Exception e) {
+			 
+			 return e.getMessage();
+		 }finally {
+			 conn.close();
+		 }
 	}
 	
 	private  Connection initHbase(String zkip,String zkport) throws IOException {
